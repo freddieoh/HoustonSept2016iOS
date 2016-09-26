e74e01225919ac8f14d341eec30894847324afbd
Fredrick Ohen <fredrickohen@Jasmines-MacBook-Pro.local>
2016-09-26 12:22:21 -0500

    IBOutlets and IBActions

---
+++ HoustonSept2016iOS/ViewController.m
@@ -0,0 +1,39 @@
+//
+//  ViewController.m
+//  HoustonSept2016iOS
+//
+//  Created by Fredrick Ohen on 9/26/16.
+//  Copyright © 2016 GeeCode. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (IBAction) submitButtonPress {
+
+    self.greetingLabel.text = self.greetingTextField.text;
+    
+
+    
+    
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
